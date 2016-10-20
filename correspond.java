f245a26bca456f1885610f193d06ea924bd4346c
gingernine <sy44895@gmail.com>
2016-10-20 13:13:14 +0900

    2016/10/20

--- correspond.java
+++ correspond.java
@@ -18,36 +18,32 @@ public class correspond{
     	String nikkeidir = "C:\\Users\\kklab\\Desktop\\yurispace\\plate_fluctuation\\src\\nikkei_needs_output";
     	String reuterdir = "C:\\Users\\kklab\\Desktop\\yurispace\\plate_fluctuation\\src\\reuter_output";
     	String datayear = "\\2006";
-    	String nikkeidata = "\\price_or_depth_change\\daily";
-    	String reuterdata = "\\daily";
-    	String writedir = "\\nikkei_reuter_match_2";
+    	String nikkeidatadir = "\\price_or_depth_change\\daily";
+    	String reuterdatadir = "\\daily";
+    	String writedir = "\\nikkei_reuter_match";
     	String writedaily = "\\daily\\";
-    	String writenikkei = "\\dismatched\\";
+    	String writedismatched = "\\dismatched\\";
     	boolean continuous = false; //ザラバ時間を判定する．
     	boolean sepsession = true; //前場と後場に分かれていた年ならtrue．
 
-    	File nikkeipath = new File(nikkeidir + datayear + nikkeidata); //読み込む日経NEEDSファイルのディレクトリのパス．
-    	File reuterpath = new File(reuterdir + datayear + reuterdata); //読み込むreuterファイルのディレクトリのパス．
+    	File nikkeipath = new File(nikkeidir + datayear + nikkeidatadir); //読み込む日経NEEDSファイルのディレクトリのパス．
+    	File reuterpath = new File(reuterdir + datayear + reuterdatadir); //読み込むreuterファイルのディレクトリのパス．
     	File[] nikkeilist = nikkeipath.listFiles(); //読み込む日経NEEDSファイル名を取得する．
     	File[] reuterlist = reuterpath.listFiles(); //読み込むreuterァイル名を取得する．
 
         for(int i=0; i<nikkeilist.length; i++){
 
-        	FileReader nikkeifr = new FileReader(nikkeilist[i]);
-            BufferedReader nikkeibr = new BufferedReader(nikkeifr);
-
             String nikkeiline = ""; //読み込む行
             String reuterline = ""; //読み込む行
             String nikkeisep[]; //読み込んだ行をカンマで区切る．
             String reutersep[]; //読み込んだ行をカンマで区切る．
-            String nikkeitrade = ""; //nikkeiの約定価格と約定数量(枚)を文字列にしたもの．
-            String reutertrade = ""; //reuterの約定価格と約定数量(枚)を文字列にしたもの．
-            String nikkeiquote = ""; //nikkeiの売り買い両方の気配値数量(枚)を文字列にしたもの．
-            String reuterquote = ""; //reuterの売り買い両方の気配値数量(枚)を文字列にしたもの．
+            String nikkeitrade = ""; //nikkeiの約定価格/数量(枚)を文字列にしたもの．
+            String reutertrade = ""; //reuterの約定価格/数量(枚)を文字列にしたもの．
+            String nikkeiquote = ""; //nikkeiの売り買い両方の気配値/数量(枚)を文字列にしたもの．
+            String reuterquote = ""; //reuterの売り買い両方の気配値/数量(枚)を文字列にしたもの．
             boolean isTrade = false; //約定データならtrue．
             boolean matched = false; //日経NEEDSとロイターのデータがマッチしたらtrue．
 			int timeflow[] = {0,0}; //日経NEEDSとロイターの収録時間が近いところの照合を行う．
-			double commatime[] = {0.0, 0.0}; //合致したデータのファイルに書き出すデータの並びが時系列に沿うようにする．
 
             //読み込むファイルの日付を抽出する．
             String[] rfilename = nikkeilist[i].getAbsolutePath().split("\\_");
@@ -57,10 +53,34 @@ public class correspond{
 
             //書き出すファイルを設定する．
             File matchedf = new File(rootdir + writedir + datayear + writedaily + rfiledate + "_.csv");
-            File dismatchedf = new File(rootdir + writedir + datayear + writenikkei + rfiledate + "_.csv");
+            File dismatchedf = new File(rootdir + writedir + datayear + writedismatched + rfiledate + "_.csv");
          	PrintWriter matchedpw = new PrintWriter(new BufferedWriter(new FileWriter(matchedf)));
          	PrintWriter dismatchedpw = new PrintWriter(new BufferedWriter(new FileWriter(dismatchedf)));
 
+         	//reuterの同日のデータファイルを開いて行数を取得する．
+    		FileReader reuterfr = new FileReader(reuterlist[i]);
+    		BufferedReader reuterbr = new BufferedReader(reuterfr);
+    		int nrow = 0; //行数をカウントする．
+         	while ((reuterline = reuterbr.readLine()) != null) {
+         		nrow++;
+         	}
+         	reuterbr.close();
+
+         	//reuterの同日のデータファイルを開いて読み込む．
+         	reuterfr = new FileReader(reuterlist[i]);
+    		reuterbr = new BufferedReader(reuterfr);
+         	String reuterdata[] = new String[nrow]; //配列をロイター社のデータを配列にしまう．
+         	int j = 0;
+         	while ((reuterline = reuterbr.readLine()) != null) {
+         		reuterdata[j] = reuterline;
+         		j++;
+         	}
+         	reuterbr.close();
+         	int start = 0; //後の配列操作の際に読み込む配列番号を指定する．
+
+         	//nikkeiの同日のデータファイルを開いて読み込む．
+         	FileReader nikkeifr = new FileReader(nikkeilist[i]);
+        	BufferedReader nikkeibr = new BufferedReader(nikkeifr);
             while ((nikkeiline = nikkeibr.readLine()) != null) {
 
             	isTrade = false; //initial.
@@ -84,23 +104,19 @@ public class correspond{
             		//ザラバ時間のみのデータを使う．
             		if(isTrade){
             			nikkeitrade = String.valueOf(Integer.parseInt(nikkeisep[3]))
-            					+ "_" + String.valueOf(Integer.parseInt(nikkeisep[4])); //約定データは"価格_数量"．
+            						+ String.valueOf(Integer.parseInt(nikkeisep[4])); //約定データは"価格数量"．
             		} else {
             			nikkeiquote = String.valueOf(Integer.parseInt(nikkeisep[5]))
-            					+ "_" + String.valueOf(Integer.parseInt(nikkeisep[6]))
-            					+ "_" + String.valueOf(Integer.parseInt(nikkeisep[7]))
-            					+ "_" + String.valueOf(Integer.parseInt(nikkeisep[8])); //気配値データは"最良買い気配数量_最良売り気配数量"．
+            						+ String.valueOf(Integer.parseInt(nikkeisep[6]))
+            						+ String.valueOf(Integer.parseInt(nikkeisep[7]))
+            						+ String.valueOf(Integer.parseInt(nikkeisep[8])); //気配値データは"最良買い気配数量最良売り気配数量"．
             		}
 
-            		//ロイター社の同日のデータファイルを開いて読み込む．
-            		FileReader reuterfr = new FileReader(reuterlist[i]);
-            		BufferedReader reuterbr = new BufferedReader(reuterfr);
 
-            		while ((reuterline = reuterbr.readLine()) != null) {
+            		for (int l=start; l<nrow; l++) {
             			//合致するまで走査．
-            			reutersep = reuterline.split(",",-1);
+            			reutersep = reuterdata[l].split(",",-1);
             			timeflow[1] = Integer.parseInt(reutersep[1].split(":")[0] + reutersep[1].split(":")[1]); //ロイターの分秒を数値化．
-            			commatime[1] = Double.parseDouble(reutersep[1].split(":")[0] + reutersep[1].split(":")[1] + reutersep[1].split(":")[2]);
 
             			if(timeflow[0]-timeflow[1] > 1) {
             				//日経NEEDSの時間とロイター社の時間の差は一分以内とする．
@@ -111,35 +127,26 @@ public class correspond{
             			}
 
             			if (isTrade && reutersep[2].equals("Trade")) {
-                			reutertrade = reutersep[3] + "_" +  reutersep[4]; //約定データは"価格_数量"．
+                			reutertrade = reutersep[3] + reutersep[4]; //約定データは"価格数量"．
                 			if(nikkeitrade.equals(reutertrade)){
-                				if(commatime[0] <= commatime[1]) {
-                					//約定データでかつ数量が一致すればファイルに書き込む．
-                    				matchedpw.println(nikkeiline + ",," + reuterline);
-                    				matched = true;
-                    				commatime[0] = commatime[1];
-                    				break;
-                    			} else {
-                    				continue;
-                    			}
+                				//約定データでかつ数量が一致すればファイルに書き込む．
+                				start = l;
+                    			matchedpw.println(nikkeiline + ",," + reuterdata[l]);
+                    			matched = true;
+                    			break;
                 			}
                 		} else if (!isTrade && reutersep[2].equals("Quote")) {
-                			reuterquote = reutersep[6] + "_" + reutersep[7] + "_"
-                						+ reutersep[8] + "_" + reutersep[9]; //気配値データは"最良買い気配数量_最良売り気配数量"．
+                			reuterquote = reutersep[6] + reutersep[7]
+                						+ reutersep[8] + reutersep[9]; //気配値データは"最良買い気配数量最良売り気配数量"．
                 			if(nikkeiquote.equals(reuterquote)){
-                				if(commatime[0] <= commatime[1]) {
-                					//気配値データでかつ数量が一致すればファイルに書き込む．
-                    				matchedpw.println(nikkeiline + ",," + reuterline);
-                    				matched = true;
-                    				commatime[0] = commatime[1];
-                    				break;
-                    			} else {
-                    				continue;
-                    			}
+                				//気配値データでかつ数量が一致すればファイルに書き込む．
+                				start = l;
+                    			matchedpw.println(nikkeiline + ",," + reuterdata[l]);
+                    			matched = true;
+                    			break;
                 			}
                 		}
             		}
-            		reuterbr.close();
 
             		if (!matched) {
             			//両社のデータで合致するものがない場合はエラーとして別ファイルに書き出す．
--- extract_reuter.java
+++ extract_reuter.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
 
 //ロイターのコンマ秒データから，日次データを作成する．
 public class extract_reuter{
@@ -17,7 +21,8 @@ public class extract_reuter{
 		String wfiledir = "\\reuter_output\\";
 		String rfiledate = "";
 		String datapath = "E:\\日経平均先物\\コンマ秒データ\\2016年取得データ";
-		String[] maturity = {"03", "06", "09", "12"}; //限月．
+		String maturity[] = {"03", "06", "09", "12"}; //限月．
+		Map<String, Integer> settlementdate = new HashMap<>(); //精算日．
 
 		File rfilepath = new File(datapath); //読み込むファイルのディレクトリのパス．
 		File[] filelist = rfilepath.listFiles(); //読み込むファイル名を取得する．
@@ -29,7 +34,9 @@ public class extract_reuter{
 			String rline = ""; //読み込む行を格納する．
 			String wline = ""; //書き込む行を作成する．
 			String JNI; //期近限月の調整のため，ファイルの種類が JNIc1 か JNIc2 かを区別する．
-			String month; //月が限月であるかどうかを判定する．
+			String month; //読み込むデータの月を格納する．
+			int date; //読み込むデータの日を格納する．
+			boolean isInit = true; //はじめの行は飛ばす．
 
 			FileReader fr = new FileReader(filelist[i]);
 			BufferedReader brtxt = new BufferedReader(fr);
@@ -40,25 +47,55 @@ public class extract_reuter{
 				newdir.mkdirs();
 			}
 
+			//rfileyearに格納した年の限月の清算日を計算する．
+			Calendar cal = Calendar.getInstance();
+			int y = Integer.parseInt(rfileyear);
+			for (int j=0; j<4; j++){
+				int m = Integer.parseInt(maturity[j]);
+				for (int d=1; d<8; d++){
+					cal.set(y, m-1, d); //曜日をセット．月番号は0から11まで．
+					if(cal.get(Calendar.DAY_OF_WEEK) == 6){
+						//金曜日の数字は6．
+						settlementdate.put(maturity[j], d+7);
+						break;
+					}
+				}
+			}
+
+			System.out.println(rfileyear + settlementdate);
+
 			File file = new File(currentdir + wfiledir + "dummy.txt"); //エラー回避のダミーファイル．
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
 
 			while ((rline = brtxt.readLine()) != null) {
 
+				if(isInit){
+					isInit = false;
+					continue;
+				}
+
 				seperated = rline.split(",", -1);// 空欄がある場合にも分割数を揃えるため -1 をつける．
 				JNI = seperated[0];
 				month = seperated[2].substring(4,6);
+				date = Integer.parseInt(seperated[2].substring(6,8));
 
 				if(JNI.equals("JNIc1") && Arrays.asList(maturity).contains(month)){
-					//ロイターの期近限月のファイルで，データが限月のものであるならスキップする．
-					continue;
+					if(date < settlementdate.get(month)){
+						//ロイターの期近限月のファイルで，データが精算日前日までのものであるならスキップする．
+						continue;
+					}
+				} else if (JNI.equals("JNIc2") && Arrays.asList(maturity).contains(month)){
+					if(date >= settlementdate.get(month)){
+						//二番目に近い限月のファイルで，データが限月の精算日後のものならスキップする．
+						continue;
+					}
 				} else if (JNI.equals("JNIc2") && !Arrays.asList(maturity).contains(month)){
 					//二番目に近い限月のファイルで，データが限月のものでないならスキップする．
 					continue;
 				}
 
 				//一日ごとにファイルに書き出していく．
-				if (!rfiledate.equals(seperated[2]) && !seperated[2].equals("Date[L]")) {
+				if (!rfiledate.equals(seperated[2])) {
 					//日にちを更新して新規日付のファイルを作成する．
 					pw.close();
 					rfiledate = seperated[2];
@@ -76,9 +113,9 @@ public class extract_reuter{
 					}
 				}
 			}
+			pw.close();
 			brtxt.close();
 			fr.close();
-			pw.close();
 		}
 	}
 }
