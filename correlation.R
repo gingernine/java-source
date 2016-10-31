5b4f05a72f59de59edf9a2915966e5cde571eb11
gingernine <sy44895@gmail.com>
2016-11-01 08:06:24 +0900

    2016/11/01

--- correlation.R
+++ correlation.R
@@ -6,49 +6,49 @@ rfilename <- paste(maindir, year, "correlation.csv", sep = "", collapse = NULL)
 timespan <- c( "", "1", "2", "5", "15", "30", "60", "120", "300" )
 len <- length(timespan)
 for (i in 1:len) {
-  
-  #読み込むファイル，書き込むファイルの名前を設定する．
-  if (i == 1) {
-    rfilename <- paste(maindir, year, "correlation", timespan[i], ".csv", sep = "")
-    wfilename <- paste(maindir, year, "statistics_summary", timespan[i], ".csv", sep = "")
-    pngnamebid <- paste(maindir, year, "hist_bid", timespan[i], ".png", sep = "")
-    pngnameask <- paste(maindir, year, "hist_ask", timespan[i], ".png", sep = "")
-  } else {
-    rfilename <- paste(maindir, year, "correlation_", timespan[i], "_.csv", sep = "")
-    wfilename <- paste(maindir, year, "statistics_summary_span_", timespan[i], "_.csv", sep = "")
-    pngnamebid <- paste(maindir, year, "hist_bid_span_", timespan[i], "_.png", sep = "")
-    pngnameask <- paste(maindir, year, "hist_ask_span_", timespan[i], "_.png", sep = "")
-  }
-  
-  data <- read.csv(rfilename, sep=",", header = T)
-  
-  #基本統計量をファイルに書き出す．
-  c <- ncol(data)
-  r <- nrow(data)
-  summary <- matrix(0, nrow = c-1, ncol = 7) #結果書き出し用の行列
-  colnames(summary) <- c("Mean", "S.D.", "Median", "Kurtosis", "Skewness", "Minimum", "Maximum")
-  rownames(summary) <- colnames(data)[2:c]
-  for (i in 2:c) {
-    Mean <- mean(data[,i])
-    SD <- sd(data[,i])
-    Median <- median(data[,i])
-    Kurtosis <- mean((data[,i] - Mean)^4)/(SD^4)
-    Skewness <- mean((data[,i] - Mean)^3)/(SD^3)
-    line <- paste(colnames(data[i]), Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
-    summary[i-1,] <- c(Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
-  }
-  write.csv(summary, wfilename, quote = F, row.names = T)
-  
-  #相関係数のヒストグラムを作成する．
-  png(pngnamebid)
-  hist(data[,2], n=25, 
-       main = "histgram of the correlation of the series of the best bid price changes",
-       xlab = "")
-  dev.off()
-  png(pngnameask)
-  hist(data[,3], n=25, 
-       main = "histgram of the correlation of the series of the best ask price changes",
-       xlab = "")
-  dev.off()
+    
+    #読み込むファイル，書き込むファイルの名前を設定する．
+    if (i == 1) {
+        rfilename <- paste(maindir, year, "correlation", timespan[i], ".csv", sep = "")
+        wfilename <- paste(maindir, year, "statistics_summary", timespan[i], ".csv", sep = "")
+        pngnamebid <- paste(maindir, year, "hist_bid", timespan[i], ".png", sep = "")
+        pngnameask <- paste(maindir, year, "hist_ask", timespan[i], ".png", sep = "")
+    } else {
+        rfilename <- paste(maindir, year, "correlation_", timespan[i], "_.csv", sep = "")
+        wfilename <- paste(maindir, year, "statistics_summary_span_", timespan[i], "_.csv", sep = "")
+        pngnamebid <- paste(maindir, year, "hist_bid_span_", timespan[i], "_.png", sep = "")
+        pngnameask <- paste(maindir, year, "hist_ask_span_", timespan[i], "_.png", sep = "")
+    }
+    
+    data <- read.csv(rfilename, sep=",", header = T)
+    
+    #基本統計量をファイルに書き出す．
+    c <- ncol(data)
+    r <- nrow(data)
+    summary <- matrix(0, nrow = c-1, ncol = 7) #結果書き出し用の行列
+    colnames(summary) <- c("Mean", "S.D.", "Median", "Kurtosis", "Skewness", "Minimum", "Maximum")
+    rownames(summary) <- colnames(data)[2:c]
+    for (i in 2:c) {
+        Mean <- mean(data[,i])
+        SD <- sd(data[,i])
+        Median <- median(data[,i])
+        Kurtosis <- mean((data[,i] - Mean)^4)/(SD^4)
+        Skewness <- mean((data[,i] - Mean)^3)/(SD^3)
+        line <- paste(colnames(data[i]), Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
+        summary[i-1,] <- c(Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
+    }
+    write.csv(summary, wfilename, quote = F, row.names = T)
+    
+    #相関係数のヒストグラムを作成する．
+    png(pngnamebid)
+    hist(data[,2], n=25, 
+         main = "histgram of the correlation of the series of the best bid price changes",
+         xlab = "")
+    dev.off()
+    png(pngnameask)
+    hist(data[,3], n=25, 
+         main = "histgram of the correlation of the series of the best ask price changes",
+         xlab = "")
+    dev.off()
 }
 
--- statistics_limit_order.R
+++ statistics_limit_order.R
@@ -13,13 +13,13 @@ summary <- matrix(0, nrow = c-1, ncol = 7)
 colnames(summary) <- c("Mean", "S.D", "Median", "Kurtosis", "Skewness", "Minimum", "Maximum")
 rownames(summary) <- colnames(data)[2:c]
 for (i in 2:c) {
-	Mean <- mean(data[,i])
-	SD <- sd(data[,i])
-	Median <- median(data[,i])
-	Kurtosis <- mean((data[,i] - Mean)^4)/(SD^4)
-	Skewness <- mean((data[,i] - Mean)^3)/(SD^3)
-	line <- paste(colnames(data[i]), Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
-	summary[i-1,] <- c(Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
+    Mean <- mean(data[,i])
+    SD <- sd(data[,i])
+    Median <- median(data[,i])
+    Kurtosis <- mean((data[,i] - Mean)^4)/(SD^4)
+    Skewness <- mean((data[,i] - Mean)^3)/(SD^3)
+    line <- paste(colnames(data[i]), Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
+    summary[i-1,] <- c(Mean, SD, Median, Kurtosis, Skewness, min(data[,i]), max(data[,i]))
 }
 write.csv(summary, wfilename, quote = F, row.names = T)
 
@@ -31,10 +31,10 @@ pngnames <- c( "Arrival_Frequency_of_Market_Buy_Orders", "Arrival_Frequency_of_M
                "Upmovement_Times_of_the_Best_Ask", "Downmovement_Times_of_the_Best_Ask" )
 
 for (i in 1:length(pngnames)) {
-  pngname <- paste(maindir, currentdir, pngnames[i], ".png", sep = "", collapse = NULL)
-  png(pngname)
-  hist(data[, i + 1], n=25, 
-       main = pngnames[i], 
-       xlab = "")
-  dev.off()
+    pngname <- paste(maindir, currentdir, pngnames[i], ".png", sep = "", collapse = NULL)
+    png(pngname)
+    hist(data[, i + 1], n=25, 
+         main = pngnames[i], 
+         xlab = "")
+    dev.off()
 }
