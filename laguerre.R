aa2b153b010072c0d5255a6d47df7ef51527ffbf
gingernine <sy44895@gmail.com>
2017-01-07 08:36:09 +0900

    2017/01/07

---
+++ laguerre.R
@@ -0,0 +1,36 @@
+
+combination <- function(n, r) {
+    if (r==0) {
+        return(1)
+    } else {
+        i <- 0:(r-1)
+        ret <- prod((n-i) / (i+1))
+        return(ret)
+    }
+}
+
+factorial <- function(l) {
+    if (l==0) {
+        return(1)
+    } else if (l > 0) {
+        return(prod(1:l))
+    }
+}
+
+laguerre <- function(x, n) {
+    s <- 0
+    for (r in 0:n) {
+        s <- s + (-1)^r * combination(n, r) * factorial(n) / factorial(r) * x^r
+    }
+    return(s * exp(-x/2) / factorial(n))
+}
+
+par(new=F)
+for (n in 1:4) {
+    curve(laguerre(x, n), xlim = c(-1, 30), ylim = c(-1, 1), col=n)
+    par(new=T)
+}
+
+legend("topright",
+       col=1:6, legend=c("n=1", "n=2", "n=3", "n=4", "n=5", "n=6"),
+       lty = c(1,1,1,1,1,1))
